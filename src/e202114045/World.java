ecbe1914390c452e76685d2eedfb134b5c389484
db-TWLV-1212 <skhu202114045@gmail.com>
2026-05-13 11:28:10 +0900

    five

--- src/e202114045/World.java
+++ src/e202114045/World.java
@@ -1,5 +1,10 @@
+package e202114045;
+
 public class World {
     public static void run() {
         System.out.println("e202114045");
+    }
+    public static void run2() {
+        System.out.println("hello world");
     }
 }
